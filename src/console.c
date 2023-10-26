9b4adfe9bb711f4d5727fc941b278879357250ab
robertcotofana <33556095+robertcotofana@users.noreply.github.com>
2023-10-26 16:30:37 +0300

    Added power switch, battery and boost regulator

--- src/console.c
+++ src/console.c
@@ -4,36 +4,36 @@
 #define LedPort 		PORTD
 #define LedDDR  		DDRD
 
-#define ColPort 		PORTA
-#define ColDDR  		DDRA
+//#define ColPort 		PORTA
+//#define ColDDR  		DDRA
 
-#define LedData 		PD0
-#define ShRegClk 		PD1 
-#define ShRegClear 		PD2 // keep HIGH
-#define Latch 			PD3
-#define EN 				PD4 // keep LOW
+#define LedData 		PD2
+#define ShRegClk 		PD5
+#define ShRegClear 		PD6 // keep HIGH
+#define Latch 			PD4
+#define EN 				PD3 // keep LOW
 
-#define Bz 				PD7
+//#define Bz 				PD7
 
-#define BtnPort			PORTC
-#define BtnDDR			DDRC
+//#define BtnPort			PORTC
+//#define BtnDDR			DDRC
 
-#define UpBtn			PC2
-#define LeftBtn			PC3
-#define RightBtn		PC4
-#define DownBtn			PC5
-#define OkBtn			PC6
-#define EscBtn			PC7
+//#define UpBtn			PC2
+//#define LeftBtn			PC3
+//#define RightBtn		PC4
+//#define DownBtn			PC5
+//#define OkBtn			PC6
+//#define EscBtn			PC7
 
 void main() {
 	//init ports
 	
-	BtnDDR = 0b00000011;
+	//BtnDDR = 0b00000011;
 	
-	ColDDR = 0b11111111;
-	ColPort = 0b10000000;
+	//ColDDR = 0b11111111;
+	//ColPort = 0b00000000;
 	
-	LedDDR = (1 << LedData)|(1 << ShRegClk)|(1 << ShRegClear)|(1 << Latch)|(1 << EN)|(1 << Bz);
+	LedDDR = (1 << LedData)|(1 << ShRegClk)|(1 << ShRegClear)|(1 << Latch)|(1 << EN);
 	
 	LedPort |= 1 << ShRegClear;
 	LedPort &= (~(1 << EN));
@@ -46,9 +46,15 @@ void main() {
 	}
 	*/
 	
+
+	//setPixel(1,1,7,7);
+	
+	
 	while(1) {
-		uint8_t red = 0b10101010;
-		uint8_t green = 0b01010101;
+		
+		uint8_t red = 0b00000001;
+		uint8_t green = 0b01110111;
+		uint8_t blue = 0b01110111;
 		
 		for (uint8_t i = 0; i < 8; i++) {
 			if (red & 0b10000000) {
@@ -84,16 +90,71 @@ void main() {
 			green = green << 1;
 		}
 		
+		for (uint8_t i = 0; i < 8; i++) {
+			if (blue & 0b10000000) {
+				// output high
+				LedPort |= 1 << LedData;
+			} else {
+				//output low
+				LedPort &= (~(1 << LedData));
+			}
+			
+			// toggle the clock
+			LedPort |= (1 << ShRegClk);
+			_delay_ms(1);
+			LedPort &= (~(1 << ShRegClk));
+			_delay_ms(1);
+			blue = blue << 1;
+		}
+		
 		// latch the shift register
 		LedPort |= (1 << Latch);
-		_delay_ms(1);
+		_delay_ms(10);
 		LedPort &= (~(1 << Latch));
-		_delay_ms(1);
+		_delay_ms(10);
 		
-		ColPort = ColPort >> 1;
-		_delay_ms(100);
+		//ColPort = ColPort >> 1;
+		//_delay_ms(100);
 		
-		if (ColPort == 0) ColPort = 0b10000000;
+		//if (ColPort == 0) ColPort = 0b10000000;
 	}
 	
 }
+
+/*
+void setPixel(uint8_t row1, uint8_t col1, uint8_t row2, uint8_t col2) {
+	// take row and col values and turn on the led
+	
+	uint16_t rowData = 0; 
+	rowData = (1 << row1)|(1 << row2);
+	
+	for (uint16_t i = 0; i < 16; i++) {
+		if (i & rowData) {
+			// output high
+			LedPort |= 1 << LedData;
+		} else {
+			//output low
+			LedPort &= (~(1 << LedData));
+		}
+		
+		// toggle the clock
+		LedPort |= (1 << ShRegClk);
+		_delay_ms(1);
+		LedPort &= (~(1 << ShRegClk));
+		_delay_ms(1);
+		
+		rowData = rowData << 1;
+	}
+	
+	// latch the shift register
+	LedPort |= (1 << Latch);
+	_delay_ms(1);
+	LedPort &= (~(1 << Latch));
+	_delay_ms(1);
+	
+	ColPort = (1 << col1);
+	ColPort |= (1 << col2);
+	
+	
+}
+*/
